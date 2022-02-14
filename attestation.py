09b5991007c7e7b8b076959d4f86e11de203016b
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-14 22:38:41 +0300

    Update attestation.py

--- attestation.py
+++ attestation.py
@@ -11,3 +11,18 @@ from pyxlsb import open_workbook as open_xlsb
 
 st.title('Отчет о выполнении проекта')
 st.header('Описание набора данных')
+
+# Создаём связь
+conn = connect()
+
+# Подготовка к SQL-запросу в Google Sheet.
+# Использует метод st.cache для повторного запуска только при изменении запроса или через 600  секунд.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+tab = pd.DataFrame(rows)
+st.write(tab)
