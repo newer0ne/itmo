9623ff000ee0468b8eddced5eb4f9e079dfe5bad
Newer0ne <t80448@gmail.com>
2022-11-01 20:19:23 +0600

    1

--- attestation.py
+++ attestation.py
@@ -1,13 +1,6 @@
 import streamlit as st
 from gsheetsdb import connect
 import pandas as pd
-import numpy as np
-import openpyxl
-import io
-from io import BytesIO
-import os
-import csv
-from pyxlsb import open_workbook as open_xlsb
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.style.use('ggplot')
